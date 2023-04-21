542950df77f1a24c3278b42b06295addc75de250
Your Name <you@example.com>
2023-04-21 19:41:09 -0400

    Agregamos funcion mostrarMenu() y en el main agregamos la estructura del menu con el uso de do-while y switch.

--- main.c
+++ main.c
@@ -35,11 +35,89 @@ int is_equal_string(void * key1, void * key2) {
     return 0;
 }
 
+void mostrarMenu()
+{
+    puts("========================================");
+    puts("             Menu Jugadores:");
+    puts("========================================");
+
+    puts("1. Crear Jugador");
+    puts("2. Mostrar Jugador");
+    puts("3. Agregar Item a Jugador");
+    puts("4. Eliminar Item de Jugador");
+    puts("5. Agregar Puntos de Habiliad a Jugador");
+    puts("6. Mostrar Jugador con Item especifico");
+    puts("7. Deshacer Ultima Accion de Jugador");
+    puts("8. Exportar datos de Jugador a Archivo de Texto"); // Sin implementar
+    puts("9. Cargar datos de Jugador desde Archivo de Texto");
+    puts("10. Salir");
+    puts("========================================");
+}
 
 
+int main(int argc, const char * argv[])
+{
+    int opcion;
 
+    /*  Aqui Creamos el mapa para guardar jugadores*/
+    Map* jugadores = createMap(is_equal_string);
+    
+    system("cls");
+    puts("========================================");
+    puts("               Trabajo 2");
+    puts("     Desarrollado por Grupo ERROR 404");
+    puts("========================================");
+    sleep(2);
 
-int main(int argc, const char * argv[]) {
+    do{
+        system("cls");
+        mostrarMenu();
+        puts("");
+        printf("Eliga una opcion: ");
+        scanf("%d", &opcion);
+        while(getchar() != '\n');
+        
+        switch (opcion)
+        {
+            case 1:
 
-    return 0;
+                break;
+            case 2:
+                
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+                
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 9:
+
+                break;
+            case 10:
+                system("cls");
+                printf("Saliendo...\n");
+                sleep(2);
+                system("cls");
+                break;
+            default:
+                printf("\nOpcion no valida. Intente nuevamente.\n\n");
+                system("pause");
+        }
+   } while (opcion != 10);
+
+    return EXIT_SUCCESS;
+    
 }
